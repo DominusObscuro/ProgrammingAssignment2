2ff20e84194f00168cf55d445a3d2be759a1aed8
Damiano Mauro <damiano@hawaii.edu>
2015-06-18 15:15:12 -1000

    Assignment 2
    For R coursera
    3a:3b:de:66:77:06:61:62:64:75:25:51:68:d8:3f:1c
    Added on Jun 18, 2015 — Last used within the last day

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+#it sets a function, gets a function, sets the inverse and gets the inverse.
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<NULL
+  
+  #set function of matrix
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  
+  #get function of matrix
+  get<-function()x
+  
+  #set inverse
+  setinverse<-function(inverse) inv<<-inverse
+  
+  #get inverse
+  getinverse<-function() inv
+  
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+  
 }
 
 
 ## Write a short comment describing this function
+#it decides if it's inversed, if yes it retrieves the information, if not it calculates the new information
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  #is inverse computed?
+  inv<-x$getinverse()
+  
+  #if yes
+  if(!is.null(inv)) {
+    #retrieve computed inverse
+    message("getting cached data")
+    return(inv)
+  }
+  
+  #if not get matrix
+  data<-x$get()
+  
+  #find inverse
+  inv<-solve(data, ...)
+  
+  #cache result
+  x$setinverse(inv)
+  
+  (return)inv
 }
